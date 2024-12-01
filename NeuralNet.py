51b9290cf627d3faf63ea2c22205ac2ba7bb8b69
Danie1Arias <daniel.arias@estudiants.urv.cat>
2024-12-01 15:51:46 +0100

    Calculate total error and prediction functions

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,5 +1,4 @@
 import numpy as np
-import random
 
 class NeuralNet:
   def __init__(self, layers, epochs, learning_rate, momentum, fact):
@@ -48,16 +47,34 @@ class NeuralNet:
         return lambda x: 1 - x ** 2
       
 
-  def fit(self, X, y):
-    rows, cols = X.shape
+  def fit(self, X_train, y_train, X_val, y_val):
+    rows, cols = X_train.shape
 
     for epoch in range(self.epochs):
         epoch_errors = []
 
         for row in range(rows):
-            self._feed_forward(X[row])
-            self._backpropagate(y[row])
-            self._update_weights()
+            self._feed_forward(X_train[row])
+            self._backpropagate(y_train[row])
+            self._update_weights_and_thresholds()
+
+        train_errors = self._calculate_total_error(X_train, y_train)
+        epoch_errors.append(np.mean(train_errors))
+
+        val_errors = None
+        if X_val.shape[0] > 0:
+           val_errors = self._calculate_total_error(X_val, y_val)
+           epoch_errors.append(np.mean(val_errors))
+        else:
+           epoch_errors.append(0)
+           val_errors = []
+
+        self.training_error.append(np.mean(train_errors))
+        self.validation_error.append(np.mean(val_errors) if val_errors is not None else 0)
+
+        max_len = min(len(self.training_error), len(self.validation_error), len(epoch_errors))
+        epoch_errors += [0] * (max_len - len(epoch_errors))
+        self.validation_error += [0] * (max_len - len(self.validation_error))
 
 
   def _feed_forward(self, X):
@@ -75,7 +92,7 @@ class NeuralNet:
       self.delta[l] = self.d_fact(self.xi[l]) * np.dot(self.w[l + 1].T, self.delta[l + 1])
 
 
-  def _update_weights(self):
+  def _update_weights_and_thresholds(self):
     for l in range(1, self.L):
         self.d_w[l] = -self.eta * np.outer(self.delta[l], self.xi[l - 1]) + self.alpha * self.d_w_prev[l]
         self.d_theta[l] = self.eta * self.delta[l] + self.alpha * self.d_theta_prev[l]
@@ -83,6 +100,24 @@ class NeuralNet:
         self.theta[l] += self.d_theta[l]
         self.d_w_prev[l] = self.d_w[l]
         self.d_theta_prev[l] = self.d_theta[l]
+
+  
+  def _calculate_total_error(self, X, y):
+    errors = []
+    for i in range(X.shape[0]):
+        self._feed_forward(X[i])
+        error = 0.5 * np.sum((self.xi[self.L - 1] - y[i]) ** 2)
+        errors.append(error)
+    return errors
+
+  def predict(self, X):
+    predictions = []
+    for sample in X:
+        self._feed_forward(sample)
+        prediction = self.xi[self.L - 1].copy()
+        prediction[np.isnan(prediction)] = 0
+        predictions.append(prediction)
+    return np.array(predictions)
 
 
   def loss_epochs(self):
